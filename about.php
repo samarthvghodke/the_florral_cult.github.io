9a6287139c45eaffc5c599b7dc7575afff15ba57
samarthvghodke <65481497+samarthvghodke@users.noreply.github.com>
2020-08-30 22:19:32 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css" type="text/css"> 
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@1&display=swap" rel="stylesheet">
+
+ 
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">THE FLORAL CULTURE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+<div class="jumbotron">
+  <h1>The Floral Culture</h1>
+  <p>Spread Enlightment By Planting Trees</p>
+</div>
+
+<section class="my-5">
+    <div class="py-5">
+        <h3 class="text-center">About US</h3>
+</div>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+        <img src="img9.jpg" class="img-fluid aboutimg">
+
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+        <h2 class="display-4">We Are The Floral Culture</h2>
+        <p  class="py-5">
+        The amazing world of plants is filled with exciting and fun facts that are waiting to be explored. If you are keen to fire your child’s 
+        imagination with interesting information, get ready to get surprised by plants facts even you aren’t aware of.
+        </p>
+        <a href="about.php" class="btn btn-success">MORE INFORMATION ABOUT US</a>
+
+    </div>
+</div>
+</div>
+</section>
+
+
+<footer>
+    <h3 class="p-3 bg-dark text-white text-center">@the_floral_cult</h3>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css" type="text/css"> 
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@1&display=swap" rel="stylesheet">
+
+ 
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">THE FLORAL CULTURE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img8.jpg" alt="Los Angeles" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="img6.jpg" alt="Chicago" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="img1.jpg" alt="New York" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+    <div class="py-5">
+        <h3 class="text-center">About US</h3>
+</div>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+        <img src="img9.jpg" class="img-fluid aboutimg">
+
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+        <h2 class="display-4">We Are The Floral Culture</h2>
+        <p  class="py-5">
+        The amazing world of plants is filled with exciting and fun facts that are waiting to be explored. If you are keen to fire your child’s 
+        imagination with interesting information, get ready to get surprised by plants facts even you aren’t aware of.
+        </p>
+        <a href="about.php" class="btn btn-success">MORE INFORMATION ABOUT US</a>
+
+    </div>
+</div>
+</div>
+</section>
+
+
+
+<section class="my-5">
+    <div class="py-5">
+        <h3 class="text-center">Our Services</h3>
+        </div>    
+
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-lg-4 col-md-4 col-12">
+              <div class="card" >
+                 <img class="card-img-top" src="img10" alt="Card image">
+             <div class="card-body">
+                  <h4 class="card-title">Birthday Gift</h4>
+                     <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">See Profile</a>
+               </div>
+                 </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-12">
+              <div class="card" >
+                 <img class="card-img-top" src="img10" alt="Card image">
+             <div class="card-body">
+                  <h4 class="card-title">Birthday Gift</h4>
+                     <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">See Profile</a>
+               </div>
+                 </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-12">
+              <div class="card" >
+                 <img class="card-img-top" src="img10" alt="Card image">
+             <div class="card-body">
+                  <h4 class="card-title">Birthday Gift</h4>
+                     <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">See Profile</a>
+               </div>
+                 </div>
+              </div>
+          </div>
+      </div> 
+</section>
+<section class="my-5">
+    <div class="py-5">
+        <h3 class="text-center">Gallery</h3>
+        </div>  
+
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="gimg1.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="gimg2.jpg" class="img-fluid pb-4">
+                </div>
+                
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="gimg3.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="gimg4.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="gimg5.jpg" class="img-fluid pb-4">
+                </div>
+                
+                
+                
+            </div>
+        </div>
+</section>
+
+<section class="my-5">
+    <div class="py-5">
+        <h3 class="text-center">cdc</h3>
+        </div>
+
+        <div class="w-50 m-auto">
+            <form action="userinfo.php" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="user" autocomplete="off" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" name="email" autocomplete="off" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Mobile</label>
+                    <input type="text" name="mobile" autocomplete="off" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Comments</label>
+                    <textarea class="form-control" name="comments"></textarea>
+                </div>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+        </div>
+</section>  
+
+
+
+<footer>
+    <h3 class="p-3 bg-dark text-white text-center">@the_floral_cult</h3>
+</footer>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
